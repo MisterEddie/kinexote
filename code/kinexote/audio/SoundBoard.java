9337d40f2c35664cd03df374efbefc6f3ae781f8
wesleyyu14 <33070748+wesleyyu14@users.noreply.github.com>
2020-02-09 05:54:03 -0500

    finished functions except FFT and waveform

--- code/kinexote/audio/SoundBoard.java
+++ code/kinexote/audio/SoundBoard.java
@@ -4,24 +4,75 @@ import java.util.ArrayList;
 
 import ddf.minim.Minim;
 
+import Math;
+
 public class SoundBoard {
 	
-	private Minim minim;
+	Minim minim;
+	AudioInput    in;
+	AudioOutput   out;
+	AudioRecorder recorder;
+  	FilePlayer    filePlayer;
+  	Sampler       sampler;
+  	MultiChannelBuffer sampleBuffer;
+  	Delay         delay1;
+  	MoogFilter    filter1;
+  	Gain          gain1;
+  	Bypass        bypassedDelay;
+  	Bypass        bypassedFilter;
+  	Bypass        bypassedGain;
 
-	public SoundBoard(Minim minim) {
-		this.minim = minim;
-	}
+	public SoundBoard(Minim minim, AudioInput in, AudioOutput out, 
+                AudioRecorder recorder, String fileName) {
+	    this.minim = minim;
+	    this.in = in;
+	    this.out = out;
+	    this.recorder = recorder;
+	    this.fileName = fileName;
+	    delay1 = new Delay( 0.07, 0.8, true, true );
+	    filter1 = new MoogFilter(500, 0.5, MoogFilter.Type.LP);
+	    gain1 = new Gain(5);
+	    sampleBuffer = new MultiChannelBuffer( 1, 1024 );
+	    sampler = new Sampler( sampleBuffer, 44100, 1 );
+	    bypassedDelay = new Bypass<Delay>( delay1 );
+	    bypassedDelay.activate();
+	    bypassedFilter = new Bypass<MoogFilter>( filter1 );
+	    bypassedFilter.activate();
+	    bypassedGain = new Bypass<Gain>( gain1 );
+	    bypassedGain.activate();
+	    sampler.patch(bypassedDelay).patch(bypassedFilter).patch(bypassedGain).patch(this.out);
+	  }
 	
 	public void play() {
+		sampler.trigger();
 		System.out.println("Play");
 	}
 
-	public void rec() {
-		System.out.println("Rec");
+	public void record() {
+		if (recorder.isRecording() ) {
+        	recorder.endRecord();
+        	System.out.println("Stopped recording")
+      	} else {
+        	recorder.beginRecord();
+        	System.out.println("Recording")
+      	}
 	}
 
-	public void stop() {
-		System.out.println("Stop");
+	public void recordState() {
+		if (recorder.isRecording()) {
+	      	System.out.println("Currently recording...");
+	    } else {
+	      	System.out.println("Not recording.");
+	    }
+	}
+
+	public void save() {
+		recorder.save();
+		float sampleRate = this.minim.loadFileIntoBuffer(fileName, sampleBuffer);
+    	int correctBufferSize = sampleBuffer.getBufferSize();
+    	sampler.setSample(sampleBuffer, sampleRate);
+    	sampler.end.setLastValue(correctBufferSize-1);
+		System.out.println("Done Saving");
 	}
 
 
@@ -30,7 +81,11 @@ public class SoundBoard {
 	 * @param gain 0-127
 	 */
 	public void setGain(int gain) {
-		System.out.printf("Gain: %d\n", gain);
+		gain /= 4.2333333333;
+		gain -= 15;
+
+		System.out.printf("Gain: %f dB\n", gain);
+		gain1.setValue(gain);
 	}
 
 
@@ -39,15 +94,19 @@ public class SoundBoard {
 	 * @param reverb 0-127
 	 */
 	public void setReverb(int reverb) {
-		System.out.printf("Reverb: %d\n", reverb);
+		reverb /= 141.1;
+		System.out.printf("Reverb: %f factor \n", reverb);
+		delay1.setDelAmp(reverb)
 	}
 
 	/**
 	 * 
-	 * @param filt 0-127
+	 * @param freq 0-127
 	 */
-	public void setFilt(int filt) {
-		System.out.printf("Filt: %d\n", filt);
+	public void setFilterFreq(int freq) {
+		freq = 20 * Math.pow(10, freq/42.4)
+		System.out.printf("Cutoff Freq: %d Hz\n", freq);
+		filter1.frequency.setLastValue(freq)
 	}
 
 	public ArrayList<Float> getWav() {
@@ -64,3 +123,5 @@ public class SoundBoard {
 		return wav;
 	}
 }
+
+
